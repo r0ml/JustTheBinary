cf9f69020b30872722777072db4fc379d8c3916b
Robert Lefkowitz <r0ml@mac.com>
2025-12-09 15:55:44 -0500

    upgraded opencv to 4.12.0

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
   ],
   targets: [
      .binaryTarget(name: "OpenCV",
-                  url: "https://github.com/r0ml/JustTheBinary/releases/download/4.10.3/OpenCV.xcframework.zip",
-                  checksum: "1afb9712bbc9b3063118d1978b2e9b0e65a5c9bfaa828650c162d37a7e021177"),
+                  url: "https://github.com/r0ml/JustTheBinary/releases/download/4.12.0/OpenCV.xcframework.zip",
+                  checksum: "01317ee5d638c7e7947194f722c46efae22cfe1fc30ffea65b657aa690783b0c"),
   ]
 )
 
