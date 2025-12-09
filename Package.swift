a3727aeba2d68335c94b3685ece09a4c98e1662d
Robert Lefkowitz <r0ml@mac.com>
2025-12-09 17:00:19 -0500

    fixed the shallow bundle issue with XCode 26

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
   ],
   targets: [
      .binaryTarget(name: "OpenCV",
-                  url: "https://github.com/r0ml/JustTheBinary/releases/download/4.12.0/OpenCV.xcframework.zip",
-                  checksum: "01317ee5d638c7e7947194f722c46efae22cfe1fc30ffea65b657aa690783b0c"),
+                  url: "https://github.com/r0ml/JustTheBinary/releases/download/4.12.1/OpenCV.xcframework.zip",
+                  checksum: "6bd5a5b7a3cb0a69018c007e8d766409d3ff788ab6df2568a9284c7131cb3ca7"),
   ]
 )
 
