01eb1a15ac17da091a48ad7652cc18871afec585
Robert Lefkowitz <r0ml@mac.com>
2026-08-25 12:37:06 -0400

    Fixed Catalyst build

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
   ],
   targets: [
      .binaryTarget(name: "OpenCV",
-                  url: "https://github.com/r0ml/JustTheBinary/releases/download/5.0.2/OpenCV.xcframework.zip",
-                  checksum: "7ceb3af163b910954ae72c671b15df7cc80beef4e03d62a433f89c90330f0096"),
+                  url: "https://github.com/r0ml/JustTheBinary/releases/download/5.0.3/OpenCV.xcframework.zip",
+                  checksum: "a075edb0c8d5ab2a51aa29364a42283e39cad5e89fd12518c3fd3355b29fd5b5"),
   ]
 )
 
