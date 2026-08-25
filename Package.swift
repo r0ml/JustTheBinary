06600bae82310f1476afc5d293ed87d69e9e5280
Robert Lefkowitz <r0ml@mac.com>
2026-08-25 17:50:18 -0400

    trying to fix the catalyst problem

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
   ],
   targets: [
      .binaryTarget(name: "OpenCV",
-                  url: "https://github.com/r0ml/JustTheBinary/releases/download/5.0.3/OpenCV.xcframework.zip",
-                  checksum: "a075edb0c8d5ab2a51aa29364a42283e39cad5e89fd12518c3fd3355b29fd5b5"),
+                  url: "https://github.com/r0ml/JustTheBinary/releases/download/5.0.5/OpenCV.xcframework.zip",
+                  checksum: "ae2ea15c24d8d3edce84c8c53aeb52dab44ac8c87784638fb3998730fccb4586"),
   ]
 )
 
