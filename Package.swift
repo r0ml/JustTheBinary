1d9d4016722e9cfb8bf9e89d01d35946bd2edfe9
Robert Lefkowitz <r0ml@mac.com>
2026-08-25 17:48:52 -0400

    trying to fix the catalyst problem

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 6.0 
+// swift-tools-version: 6.0
 
 import PackageDescription
 
 let package = Package(
   name: "OpenCV",
   platforms: [
-    .macOS(.v15), .iOS(.v1r8), .macCatalyst(.v18)
+    .macOS(.v15), .iOS(.v18), .macCatalyst(.v18)
   ],
   products: [
     .library(name: "OpenCV",
@@ -14,8 +14,8 @@ let package = Package(
   ],
   targets: [
      .binaryTarget(name: "OpenCV",
-                  url: "https://github.com/r0ml/JustTheBinary/releases/download/5.0.2/OpenCV.xcframework.zip",
-                  checksum: "7ceb3af163b910954ae72c671b15df7cc80beef4e03d62a433f89c90330f0096"),
+                  url: "https://github.com/r0ml/JustTheBinary/releases/download/5.0.5/OpenCV.xcframework.zip",
+                  checksum: "ae2ea15c24d8d3edce84c8c53aeb52dab44ac8c87784638fb3998730fccb4586"),
   ]
 )
 
